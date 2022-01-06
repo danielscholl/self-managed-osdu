752abf6efe7fc4dd5500011ffe73b44ab6a0fc4f
danielscholl <degnome@me.com>
2022-01-06 19:58:36 +0000

    API definition transferred by SwaggerHub

--- src/sdk/python/swagger_client/models/__init__.py
+++ src/sdk/python/swagger_client/models/__init__.py
@@ -31,6 +31,9 @@ from swagger_client.models.file_legal import FileLegal
 from swagger_client.models.file_link_list import FileLinkList
 from swagger_client.models.file_meta_item import FileMetaItem
 from swagger_client.models.file_metadata_response import FileMetadataResponse
+from swagger_client.models.file_relationships import FileRelationships
+from swagger_client.models.file_to_many_relationship import FileToManyRelationship
+from swagger_client.models.file_to_one_relationship import FileToOneRelationship
 from swagger_client.models.legal_tag_dto import LegalTagDto
 from swagger_client.models.legal_tag_dtos import LegalTagDtos
 from swagger_client.models.legal_tag_invalid_response import LegalTagInvalidResponse
