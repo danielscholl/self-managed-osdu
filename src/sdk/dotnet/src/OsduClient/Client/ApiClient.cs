c6bb449f146585d43c01300f20c75e9d12c6924f
Kyle Miller <kymille@microsoft.com>
2022-01-07 18:54:13 -0500

    Updated nuget packages and fixed errors - currently builds

--- src/sdk/dotnet/src/OsduClient/Client/ApiClient.cs
+++ src/sdk/dotnet/src/OsduClient/Client/ApiClient.cs
@@ -134,7 +134,7 @@ namespace OsduClient.Client
             // add file parameter, if any
             foreach(var param in fileParams)
             {
-                request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentType);
+                request.AddFile(param.Value.Name, param.Value.Writer, param.Value.FileName, param.Value.ContentLength, param.Value.ContentType);
             }
 
             if (postBody != null) // http body (model or byte[]) parameter
@@ -203,7 +203,7 @@ namespace OsduClient.Client
                 path, method, queryParams, postBody, headerParams, formParams, fileParams,
                 pathParams, contentType);
             InterceptRequest(request);
-            var response = await RestClient.ExecuteTaskAsync(request);
+            var response = await RestClient.ExecuteAsync(request);
             InterceptResponse(request, response);
             return (Object)response;
         }
