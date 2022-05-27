49a33d4fa5e2d14a2c3e18f6d284104fbe932326
Corey Callaway <coreylcallaway@gmail.com>
2022-05-27 12:40:49 -0400

    Update main.tf

--- customizations/templates/service_resources/main.tf
+++ customizations/templates/service_resources/main.tf
@@ -277,7 +277,7 @@ resource "azurerm_role_assignment" "system_storage_data_contributor" {
 # Network
 #-------------------------------
 module "network" {
-  source = "git::https://github.com/cocallaw/tfmodule-osdu-network-existing?ref=v0.1.3"
+  source = "git::https://github.com/cocallaw/tfmodule-osdu-network-existing?ref=v0.1.4"
 
   existing_resource_group_name = local.existing_resource_group_name
   existing_vnet_name           = local.existing_vnet_name
